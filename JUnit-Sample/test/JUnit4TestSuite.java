22c0d424d40f64baac83764f84c974a49dc44bba
Reno Kriz <rekriz@vassar.edu>
2013-09-27 14:17:48 -0400

    Lab 2

---
+++ JUnit-Sample/test/JUnit4TestSuite.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author rekriz
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({UtilsJUnit4Test.class, VectorsJUnit4Test.class})
+public class JUnit4TestSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+    
+}
